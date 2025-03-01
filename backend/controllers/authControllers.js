e8c4d95b522d080f90efd8a30c82d6af1808195e
Evan-Codes88 <Evan2401tm@gmail.com>
2025-03-01 12:58:57 +1000

    Feat: Implemente Authentication Routes & Controller Skeletons

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,12 @@
+
+export const signup = async (request, response) => {
+    response.send("Sign Up Route Called");
+};
+
+export const login = async (request, response) => {
+    response.send("Login Route Called");
+};
+
+export const logout = async (request, response) => {
+    response.send("Logout Route Called");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/authRoutes.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 1234;
+
+
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, () => {
     console.log("Server running on http://localhost:" + PORT);
