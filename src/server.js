f60154890abd2e21a7e883a09ae38002d205c330
Evan-Codes88 <Evan2401tm@gmail.com>
2025-03-26 00:48:17 +1000

    Feat: Imported Cors and configured it to allow backend o communicate with frontend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -19,6 +20,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
